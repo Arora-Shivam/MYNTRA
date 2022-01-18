de7f949e448c1230ab6684b460ecf8c6e8211d31
Arora-Shivam <shiarora4002@gmail.com>
2022-01-18 12:41:39 +0530

    Jacket Data

---
+++ Men_jacket.js
@@ -0,0 +1,92 @@
+var Men_Data=[
+    {
+    image_link:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/14789756/2021/9/17/4affc9a2-819b-4ca0-8e9d-b89ce88c59381631868639389-ADIDAS-Men-Tshirts-7951631868638243-1.jpg",
+    category: "Jacket",
+    name: "Cold.RDY Training Hoodie",
+    price: 6299,
+    strikedoffprice:8999 ,
+    brandname:"ADIDAS",
+    rating:"4"
+    },
+    {
+    image_link:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/14789776/2021/8/9/4d258a49-be59-48d1-aab3-edeadad0b5b31628510035976-ADIDAS-Men-Tshirts-2751628510035315-1.jpg",
+    category: "Jacket",
+    name: "Aeromotion Hooded Jacket",
+    price: 3179,
+    strikedoffprice:5299,
+    brandname:"ADIDAS",
+    rating:"4.2"
+    },
+    {
+    image_link:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/14905088/2021/8/9/b73c5134-0523-4c39-9a17-f4eef096b3271628509212883-Nike-Repel-Miler-Mens-Running-Jacket-7841628509212123-1.jpg",
+    category: "Jacket",
+    name: "Repel Miler Running Jacket",
+    price: 3286,
+    strikedoffprice:4695,
+    brandname:"NIKE",
+    rating:"3.7"
+    },
+    {
+    image_link:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/16092732/2021/12/11/4962c407-d9cc-4998-a703-e921b3ba34c81639216950801-Nike-Dri-FIT-Mens-Woven-Training-Jacket-3731639216950255-1.jpg",
+    category: "Jacket",
+    name: "Open Front Jacket",
+    price: 2786,
+    strikedoffprice:3495,
+    brandname:"NIKE",
+    rating:"2.6"
+    },
+    {
+    image_link:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/14905050/2021/11/12/efeaf45e-8d02-42e2-ab55-8a7d174ead071636718877513-Nike-Men-Blue-Bomber-Jacket-8791636718876793-1.jpg",
+    category: "Jacket",
+    name: "Bomber Jacket",
+    price: 2791,
+    strikedoffprice:4295,
+    brandname:"NIKE",
+    rating:"4.8"
+    },
+    {
+    image_link:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/14643390/2021/7/14/358acc9a-3517-4638-8e44-c4872a7172f11626265371465-Nike-Sportswear-Mens-Unlined-Core-Track-Jacket-2491626265370-1.jpg",
+    category: "Jacket",
+    name: "Track Sporty Jacket",
+    price: 2796,
+    strikedoffprice:3995,
+    brandname:"NIKE",
+    rating:"3.5"
+    },
+    {
+    image_link:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/12944874/2020/12/1/542a5f52-cad2-49d2-89a0-14b4464465b91606798689065-ADIDAS-Men-Black-Solid-Tech-Fabric-Mix-Hoodie-Jacket-9451606-1.jpg",
+    category: "Jacket",
+    name: "Hoodie Jacket",
+    price: 3599,
+    strikedoffprice:5999,
+    brandname:"ADIDAS",
+    rating:"4.1"
+    },
+    {
+    image_link:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/14746194/2021/8/10/fe48460c-799b-4be6-b3b3-b2fde7be13151628594928210-ADIDAS-Men-Purple-EveryHike-Full-Zip-Solid-Hiking-Jacket-631-1.jpg",
+    category: "Jacket",
+    name: "Hiking Jacket",
+    price: 4599,
+    strikedoffprice:7999,
+    brandname:"ADIDAS",
+    rating:"2.8"
+    },
+    {
+    image_link:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/15629670/2021/12/24/d2e577b0-9d95-4436-808d-b025b51b841a1640335786236-Nike-Men-Blue-Brand-Logo-Printed-Standard-Fit-AIR-PK-Sustain-1.jpg",
+    category: "Jacket",
+    name: "Air PK Jacket",
+    price: 3496,
+    strikedoffprice:4995,
+    brandname:"NIKE",
+    rating:"4"
+    },
+    {
+    image_link:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/14789752/2021/9/17/75198009-d334-47c7-a0c1-4ee4dc523bc51631868631345-ADIDAS-Men-Tshirts-6811631868630783-1.jpg",
+    category: "Jacket",
+    name: "Woven Track Jacket",
+    price: 3709,
+    strikedoffprice:5299,
+    brandname:"ADIDAS",
+    rating:"3.9"
+    }
+    ];
